d3a80eff8adfa3c0821dec6d9b3bbc9c830e3741
eria-developer <eriddeveloper@gmail.com>
2024-11-05 21:14:09 +0300

    made error on purpose

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
       <GallerySection />
       <CeoMessageSection />
       <TestimonialsAndTrustSection />
-      <Footer />
+      <Footer 
     </div>
   );
 }
